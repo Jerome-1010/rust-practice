3d88abf0adbcb66d38edae656db74a74cbe456f2
Jerome-1010 <uw.p.21k@gmail.com>
2024-06-30 15:30:25 +0900

    古いパッケージのものを更新しつつechorのベースを作成

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Command, Arg, ArgAction};
+
 fn main() {
-    println!("{:#?}", std::env::args());
+    let matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1)
+                // .action(ArgAction::Set),
+        )
+        .arg(
+            Arg::new("omit_newline")
+            .short('n')
+            .help("Do not print newline")
+        )
+        .get_matches();
+
+    println!("{:#?}", matches);
 }
